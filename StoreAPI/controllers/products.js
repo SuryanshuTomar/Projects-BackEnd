b31f41c84186ae61b34fba055976a767ed2d7efb
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-18 23:03:40 +0530

    find products by name, featured and company

--- StoreAPI/controllers/products.js
+++ StoreAPI/controllers/products.js
@@ -6,8 +6,20 @@ const getAllProductsStatic = async (req, res, next) => {
 };
 
 const getAllProducts = async (req, res, next) => {
-	res.status(200).json({ msg: "Products route" });
+	const { featured, company, name } = req.query;
+	const queryObject = {};
+	if (featured) {
+		queryObject.featured = featured === "true" ? true : false;
+	}
+	if (company) {
+		queryObject.company = company;
+	}
+	if (name) {
+		queryObject.name = { $regex: name, $options: "i" };
+	}
+	console.log(queryObject);
+	const products = await Product.find(queryObject);
+	res.status(200).json({ products, nbHits: products.length });
 };
-
 
 module.exports = { getAllProductsStatic, getAllProducts };
