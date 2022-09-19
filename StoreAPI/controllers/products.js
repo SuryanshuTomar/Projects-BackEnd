de1cc143afa49e74fad0752ef0ec5ca9a239dd0f
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-20 00:11:56 +0530

    products sorting logic

--- StoreAPI/controllers/products.js
+++ StoreAPI/controllers/products.js
@@ -1,12 +1,12 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res, next) => {
-	const products = await Product.find({});
+	const products = await Product.find({}).sort("-name  price");
 	res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res, next) => {
-	const { featured, company, name } = req.query;
+	const { featured, company, name, sort } = req.query;
 	const queryObject = {};
 	if (featured) {
 		queryObject.featured = featured === "true" ? true : false;
@@ -17,8 +17,17 @@ const getAllProducts = async (req, res, next) => {
 	if (name) {
 		queryObject.name = { $regex: name, $options: "i" };
 	}
-	console.log(queryObject);
-	const products = await Product.find(queryObject);
+	// console.log(queryObject);
+	let result = Product.find(queryObject);
+
+	if (sort) {
+		const sortString = sort.split(",").join(" ");
+		result = result.sort(sortString);
+	} else {
+		result = result.sort("createdAt");
+	}
+
+	const products = await result;
 	res.status(200).json({ products, nbHits: products.length });
 };
 
